09cff9c040ecf465bf41459a5273719e1e8ed7b8
davigamer987 <68574209+davigamer987@users.noreply.github.com>
2021-09-25 12:09:25 -0400

    Upload Device Tree

---
+++ AndroidProducts.mk
@@ -0,0 +1,7 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/amy_TP1803.mk
+
+COMMON_LUNCH_CHOICES := \
+    amy_TP1803-user \
+    amy_TP1803-userdebug \
+    amy_TP1803-eng
---
+++ device-lineage.mk
@@ -0,0 +1,14 @@
+# Camera
+PRODUCT_PACKAGES += \
+    Snap
+
+# Init-Files
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/init-files/init.recovery.qcom.rc:recovery/root/init.recovery.qcom.rc
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-amy
+
+# Trust HAL
+PRODUCT_PACKAGES += \
+    lineage.trust@1.0-service
